0e0948e1cae3fbb2cdde413fea296cae6757c967
a-mcc <108814502+a-mcc@users.noreply.github.com>
2023-07-26 00:30:58 +0100

    Adding changes (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { AgGridAngular } from 'ag-grid-angular';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, ColumnState, ValueGetterParams } from 'ag-grid-community';
 import { Race } from './common/race';
-import { Result } from './common/result';
 import { ChampionChipIreland } from './providers/championchip-ireland/championchip-ireland.provider';
 import { IProvider } from './providers/provider';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
@@ -12,43 +11,31 @@ import { IProvider } from './providers/provider';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  @ViewChild('#grid') grid!: AgGridAngular;
+  @ViewChild(AgGridAngular) grid!: AgGridAngular;
 
-  public defaultColumnDef: ColDef;
-  public gridColumnDefinitions: ColDef[];
-  public gridData: Result[] = [];
+  public defaultGridColumnDef: ColDef = {
+    filter: true,
+    sortable: true,
+    flex: 1,
+  };
+  public gridColumnDefinitions: ColDef[] = [];
+  public gridData: any[] = [];
 
-  public activeProvider!: IProvider;
+  public activeProvider?: IProvider;
   public providers: IProvider[];
 
   public activeRace?: Race;
   public races: Race[] = [];
 
-  constructor(championChipIreland: ChampionChipIreland) {
+  constructor(private championChipIreland: ChampionChipIreland) {
     this.providers = [championChipIreland];
     this.onProviderChange(this.providers[0].name);
-
-    this.gridColumnDefinitions = [
-      { field: 'firstName' },
-      { field: 'lastName' },
-      { field: 'position' },
-      { field: 'bib' },
-      { field: 'gender' },
-      { field: 'category' },
-      { field: 'club' },
-      { field: 'raceTime' },
-      { field: 'chipTime' },
-    ];
-
-    this.defaultColumnDef = {
-      filter: true,
-    };
   }
 
   title = 'WPR Results';
 
   async onProviderChange(name: string): Promise<void> {
-    this.activeProvider = this.providers.find((x) => x.name === name)!;
+    this.activeProvider = this.providers.find((x) => x.name === name);
     this.races = await this.activeProvider!.getRaces();
     this.onRaceChange(this.races[0].name);
   }
@@ -57,5 +44,27 @@ export class AppComponent {
     this.activeRace = this.races.find((x) => x.name === name);
 
     this.gridData = this.activeRace!.results;
+    this.gridColumnDefinitions = Object.keys(this.gridData[0]).map((key) => {
+      const hide = !this.gridData.some((x) => x[key]);
+      const isNumeric = this.gridData.every(
+        (x) => !Number.isNaN(Number(x[key]))
+      );
+
+      return {
+        field: key,
+        hide: hide,
+        cellDataType: isNumeric ? 'number' : undefined,
+        valueGetter: isNumeric ? this.numberParser(key) : undefined,
+      };
+    });
+  }
+
+  numberParser(key: string) {
+    return (params: ValueGetterParams) => Number(params.data[key]);
+  }
+
+  resetGrid() {
+    this.grid.api.setFilterModel(null);
+    this.grid.columnApi.resetColumnState();
   }
 }
